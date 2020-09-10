28845c3e82249a6c3900d87e92feb40b73b13e8d
JackMandrake <coignard.cyrille@hotmail.fr>
2020-09-10 16:29:51 +0200

    fix: syntax error in PlaceController

--- gwith_back/src/Controller/PlaceController.php
+++ gwith_back/src/Controller/PlaceController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\ENtity\Place;
+use App\Entity\Place;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\PlaceRepository;
