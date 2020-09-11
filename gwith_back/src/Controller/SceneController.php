93db18315ae915d5048ec74010a00243fae18b9f
SoleneLivran <s.livran@gmail.com>
2020-09-11 15:22:54 +0200

    feat: remove useless code

--- gwith_back/src/Controller/SceneController.php
+++ gwith_back/src/Controller/SceneController.php
@@ -3,9 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Transition;
-use App\Repository\SceneRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use App\Entity\Scene;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
