f6faa40e879ba83b4399ca83707ce10afe0f51fb
SoleneLivran <s.livran@gmail.com>
2020-09-14 20:44:59 +0200

    feat: story_category list method

--- gwith_back/src/Repository/StoryCategoryRepository.php
+++ gwith_back/src/Repository/StoryCategoryRepository.php
@@ -18,33 +18,4 @@ class StoryCategoryRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, StoryCategory::class);
     }
-
-    // /**
-    //  * @return StoryCategory[] Returns an array of StoryCategory objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('s')
-            ->andWhere('s.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('s.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?StoryCategory
-    {
-        return $this->createQueryBuilder('s')
-            ->andWhere('s.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
