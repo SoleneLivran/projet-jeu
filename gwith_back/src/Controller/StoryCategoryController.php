9ae77c64ba1a99f2dfeac3d63a204d7818ba98d0
SoleneLivran <s.livran@gmail.com>
2020-09-14 20:50:53 +0200

    fix: add isGranted annotation

--- gwith_back/src/Controller/StoryCategoryController.php
+++ gwith_back/src/Controller/StoryCategoryController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\StoryCategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * @Route("/api/story_categories")
