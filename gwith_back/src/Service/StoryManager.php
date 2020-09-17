9c59e2ed739ba7b289d423b4de61278c6bde2cac
SoleneLivran <s.livran@gmail.com>
2020-09-17 17:14:38 +0200

    feat: WIP clear story scenes in DB before registering new ones

--- gwith_back/src/Service/StoryManager.php
+++ gwith_back/src/Service/StoryManager.php
@@ -24,6 +24,14 @@ class StoryManager
 
     public function createScenes(Story $story, array $scenesData) {
         //===========SCENES===============
+        // delete previously registered scenes, if any, for the Story update
+        $previousVersionScenes = $story -> getScenes();
+        if ($previousVersionScenes !== null) {
+            foreach ($previousVersionScenes as $previousVersionScene) {
+                $this->manager->remove($previousVersionScene);
+            }
+        }
+
         // array to keep track of the scenes we are about to create
         $scenes = [];
 
