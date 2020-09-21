4125a890d4212f32f45ef6f3ac55f0b8cd948cc8
JackMandrake <coignard.cyrille@hotmail.fr>
2020-09-21 14:41:34 +0200

    feat:rating average

--- gwith_back/src/Repository/RatingRepository.php
+++ gwith_back/src/Repository/RatingRepository.php
@@ -33,35 +33,10 @@ class RatingRepository extends ServiceEntityRepository
 
         $query = $queryBuilder->getQuery();
 
-        return $query->getOneOrNullResult;
-    }
+        $result = $query->getOneOrNullResult();
 
-    // /**
-    //  * @return Rating[] Returns an array of Rating objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('r')
-            ->andWhere('r.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('r.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
+        $story->setRating($result);
 
-    /*
-    public function findOneBySomeField($value): ?Rating
-    {
-        return $this->createQueryBuilder('r')
-            ->andWhere('r.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
+        return $result;
     }
-    */
 }
