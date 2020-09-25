6b8d801f42b1357124c91441d75449a7274cf76d
SoleneLivran <s.livran@gmail.com>
2020-09-25 15:46:39 +0200

    fix: fix after testing

--- gwith_back/src/Form/AvatarType.php
+++ gwith_back/src/Form/AvatarType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\AppUser;
 use App\Entity\Avatar;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -27,6 +29,7 @@ class AvatarType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => AppUser::class,
+            'csrf_protection' => false
         ]);
     }
 }
